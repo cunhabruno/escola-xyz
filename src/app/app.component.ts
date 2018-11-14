cb3409267047d29a97cead6ec727048ba1ac68c6
Cunha, Bruno (ESI) <bruno.cunha@adp.com>
2018-11-14 20:20:34 -0200

    Add home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'escola-xyz';
+  title = 'Escola-xyz';
+  constructor(public router: Router) {}
 }
